22035f8849946fea2e9c02de46d356a2c48ef778
Emelie Rönnlund <emelieronnlund@gmail.com>
2016-03-21 15:58:13 +0100

    Added AngularJS to the project. Remade the Calendar in HTML5 canvas and AngularJS for more control.

--- LMSProject/Controllers/CalendarTestController.cs
+++ LMSProject/Controllers/CalendarTestController.cs
@@ -9,37 +9,53 @@ namespace LMSProject.Controllers
 {
     // This is Emelies temporary controller for calendars or something.
 
+    public class CalendarViewModel
+    {
+        public int Hours { get; set; }
+        public int Minutes { get; set; }
+        public string Label { get; set; }
+        public int DayOfTheWeek { get; set; }
+    };
+
     public class CalendarTestController : Controller
     {
-
-        // GET: CalendarTest
-        [Authorize(Roles = "Teacher")]
-        public ActionResult Index()
+        List<CalendarViewModel> d;
+        public CalendarTestController()
         {
             schedule s = new schedule();
             Random ran = new Random();
-            List<scheduleDetail> d = new List<scheduleDetail>();
+            d = new List<CalendarViewModel>();
             for (int i = 0; i < 10; i++)
             {
-                scheduleDetail detail = new scheduleDetail() { startTime = DateTime.Now, scheduleDetailID = i, schedules = s, scheduleID = 0, name = "Test" };
+                scheduleDetail detail = new scheduleDetail() { startTime = DateTime.Now, scheduleDetailID = i, schedules = s, scheduleID = 0, name = "Test", endTime = DateTime.Now };
                 detail.startTime = detail.startTime.AddMinutes(ran.Next(480));
                 detail.startTime = detail.startTime.AddDays(ran.Next(7));
-                d.Add(detail);
-            }
-            //detail.startTime.AddHours(8.0f);
-            //schoolClass myClass = new schoolClass();
-            //s.schoolClasses = myClass;
-            //detail.schedules = s;
-            //List<scheduleDetail> sch = new List<scheduleDetail>();
-            //sch.Add( = detail);
-            //detail.startTime = detail.startTime.AddHours(3);
-            //sch.Add(detail);
-            //detail.startTime = detail.startTime.AddHours(1);
-            //sch.Add(detail);
-            //detail.startTime = detail.startTime.AddHours(2);
-            //sch.Add(detail);
+                detail.endTime = detail.startTime.AddMinutes(30 + ran.Next(100));
+                //detail.endTime = detail.startTime.AddHours(1);
+                CalendarViewModel cvm = new CalendarViewModel() { Hours = detail.startTime.Hour, Label = detail.name, Minutes = (int) detail.startTime.Minute, DayOfTheWeek = (int) detail.startTime.DayOfWeek};
 
+                d.Add(cvm);
+            }
+        }
+        [Authorize]
+        public JsonResult getSchedule()
+        {
+            return Json(d, JsonRequestBehavior.AllowGet);
+        }
+        // GET: CalendarTest
+        //[Authorize(Roles = "Teacher")]
+        public ActionResult Index()
+        {
             return View(d);
         }
+
+        [Authorize(Roles = "Teacher")]
+        [HttpPost]
+        public ActionResult CreateScheduleDetail(scheduleDetail model)
+        {
+            return RedirectToAction("Index");
+        }
+
+
     }
 }
